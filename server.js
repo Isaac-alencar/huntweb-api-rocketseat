6d7c0ba12ee2bb2828696651f0b59ee3ec4b9da0
Isaac Alencar <alencarisaac3@gmal.com>
2020-03-27 11:30:09 -0300

    routes and controllers for Products were added

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+//Modules
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+
+
+//Inicialização do App
 const app = express();
 
+//Conexo com o banco de dados
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+requireDir('./src/models/');
+
+const Product = mongoose.model('Product');
+
+//Criando a primeira rota (Index);
 app.get('/', (req, res) => {
+
+    Product.create({ 
+        title: 'React Native', 
+        description: 'Build native apps with React',
+        url: 'https://github.com/facebook/react-native'
+     })
+
+
+
     res.send("Hello World!")
 });
 
